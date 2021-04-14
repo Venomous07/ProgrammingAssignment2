69eae7cd94f5b17366c477638680fd55a69517a8
Venom-man <patrickramirez304@gmail.com>
2021-04-14 18:53:47 +0700

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Gives correct answer for matrix
+## Computing Square Matrix
 
-## Write a short comment describing this function
+## Special Vector Function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+	inv <- NULL                      #null will inverse be initialize
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function()x            #where can get matrix x function
+  setinv <- function(inverse) {inv <<- inverse}
+  getinv <- function() {
+    inver<-ginv(x)
+    inver%*%x                   #will help inverse of matrix be obtain
+  }               				
+  list(set = set, get = get, 
+       setinv = setinv, 
+       getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## Calculate mean set data value
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ...) 
+{
+	inv <- x$getinv()
+  if(!is.null(inv)){             #will cause function check inverse if NULL
+    message("I get the correct Answer")
+    return(inv)                    #will cause to that the returns value inverse
+  }
+  data<- x$get()
+  inv<- solve(data, ...)              #function helps calculate value inverse
+  x$setinv(inv)
+  inv                    ##function of inverse of 'x' will return because matrix
+       
 }
